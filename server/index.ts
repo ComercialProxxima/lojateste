c706dcd62dfd14f1ddbe80db5bae086d57339328
ComercialProxxima <anderson.silva.proxxima@gmail.com>
2025-05-14 20:49:33 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import path from "path";
 import { fileURLToPath } from 'url';
+import 'dotenv/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
