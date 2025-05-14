69321e07cb087f3bfb64928f608338245f1be862
ComercialProxxima <anderson.silva.proxxima@gmail.com>
2025-05-14 20:37:24 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,20 +1,12 @@
-import { Pool } from 'pg'; // PostgreSQL nativo
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from '@shared/schema';
- 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    'DATABASE_URL must be set. Did you forget to provision a database?',
-  );
-}
- 
-const { Pool } = import('pg');
+// db.js
+import pg from 'pg';
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // necessário para conexões SSL em serviços externos
+    rejectUnauthorized: false, // necessário no Render
   },
 });
- 
-export const db = drizzle(pool, { schema });
+
+export default pool;
