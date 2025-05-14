7c80920313aa97f4d00f19fcd8baee2afc0260a0
ComercialProxxima <anderson.silva.proxxima@gmail.com>
2025-05-14 20:43:28 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,20 @@
-// db.js
-import pg from 'pg';
-const { Pool } = pg;
+// server/db.ts
 
-const pool = new Pool({
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from 'ws';
+import * as schema from '@shared/schema';
+
+neonConfig.webSocketConstructor = ws;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    'DATABASE_URL must be set. Did you forget to provision a database?',
+  );
+}
+
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // necessário no Render
-  },
 });
 
-export default pool;
+export const db = drizzle({ client: pool, schema });
